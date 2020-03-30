6ae7ff73cb0b54df4b8aac1623052b6d471f81f2
Gabriel Onike <onikegabriel@gmail.com>
2020-03-30 08:55:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,3 @@
 def fizzbuzz
-  (1..100).each do |num|
-	message = ""
-	message << "fizz" if num%3 == 0
-	message << "buzz" if num%5 == 0
-	message << num.to_s if message.length == 0
-	puts message
+
   end
